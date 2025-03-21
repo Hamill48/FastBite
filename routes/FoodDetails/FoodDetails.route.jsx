c7ecf773133fd6a5f3392fe1504b09b44022003c
Hamill48 <davidszenyita@gmail.com>
2025-03-21 14:20:43 +0100

    Creating the FoodDetails route

--- routes/FoodDetails/FoodDetails.route.jsx
+++ routes/FoodDetails/FoodDetails.route.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Text, SafeAreaView, ScrollView, Image, View } from 'react-native';
+import { useState } from 'react';
+import { Text, SafeAreaView, ScrollView, Image, View, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useRoute } from '@react-navigation/native';
 
 import FoodDetailStyles from './FoodDetails.styles';
 
 const FoodDetail = () => {
+  const [selectedSize, setSelectedSize] = useState('size1');
+
   const route = useRoute();
   const { food } = route.params;  // Lekérjük a foodID-t a paraméterekből
 
@@ -19,16 +21,52 @@ const FoodDetail = () => {
               uri: food.image,
             }}
             style={{ width: 250, height: 250, marginBottom: 20, }}
-          />
-        <Text style={FoodDetailStyles.title}>{food.name}</Text>
-        {/* <Text style={FoodDetailStyles.foodInfo}>ID: {food.id}</Text> */}
-        <View style={FoodDetailStyles.ingredientsContainer}>
-          {food.ingredients.map((ingredient, index) => (
-            <Text key={index}>
-              {index === food.ingredients.length - 1 ? ingredient : `${ingredient}, `}
-            </Text>
-          ))}
-        </View>
+      />
+
+      <Text style={FoodDetailStyles.title}>{food.name}</Text>
+
+      <View style={FoodDetailStyles.ingredientsContainer}>
+        {food.ingredients.map((ingredient, index) => (
+          <Text key={index}>
+            {index === food.ingredients.length - 1 ? ingredient : `${ingredient}, `}
+          </Text>
+        ))}
+      </View>
+
+      <View style={FoodDetailStyles.allergensContainer}>
+        {food.allergens.map((allergen, index) => (
+          <Text key={index}>
+            {index === food.allergens.length - 1 ? allergen : `${allergen}, `}
+          </Text>
+        ))}
+      </View>
+
+      <View style={FoodDetailStyles.sizeButtonsContainer}>
+        {Object.entries(food.sizes).map(([key, value]) => {
+          const isSelected = selectedSize === key;
+
+          return(
+            <TouchableOpacity 
+              key={key} onPress={() => {setSelectedSize(key);}}       
+              style={[
+                FoodDetailStyles.sizeBox,
+                isSelected && FoodDetailStyles.sizeBoxSelected
+              ]}>
+
+              <Text style={[FoodDetailStyles.sizeText, isSelected && FoodDetailStyles.sizeTextSelected]}>{value[`${key}_name`]}</Text>
+            </TouchableOpacity>
+          );        
+        })}
+      </View>
+
+      <Text>
+        Ár: {food.sizes[selectedSize][`${selectedSize}_price`]} Ft
+      </Text>
+
+      <TouchableOpacity style={FoodDetailStyles.addToCartButton}>
+        <Text style={FoodDetailStyles.addToCartText}>KOSÁRBA TESZEM</Text>
+      </TouchableOpacity>
+
         {/* További étel adatok: név, leírás, kép, stb. */}
         <StatusBar style="auto" />
       </ScrollView>
--- routes/FoodDetails/FoodDetails.styles.tsx
+++ routes/FoodDetails/FoodDetails.styles.tsx
@@ -27,6 +27,57 @@ const FoodDetailStyles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "row", // Az elemek egymás mellett jelennek meg
     flexWrap: "wrap",
+    marginBottom: 20,
+  },
+
+  allergensContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  sizeBox: {
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#fff',
+    padding: 12,
+    marginVertical: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+
+  sizeBoxSelected: {
+    backgroundColor: '#B63130',
+    borderColor: 'darkred',
+  },
+
+  sizeText: {
+    color: '#000',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+
+  sizeTextSelected: {
+    color: '#fff',
+  },
+
+  sizeButtonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around',
+  },
+
+  addToCartButton: {
+    backgroundColor: '#B63130',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+
+  addToCartText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
